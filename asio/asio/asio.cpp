fccbfc6ba4bb25bc332adc65cd2b0b5eaf1bff29
Alex.liang <liangnan03@hotmail.com>
2020-05-18 21:25:10 +0800

    change folder name to boost

--- asio/asio/asio.cpp
+++ asio/asio/asio.cpp
@@ -3,13 +3,51 @@
 
 #include "stdafx.h"
 
+//int main()
+//{
+//    return 0;
+//}
+
+
+#include <ctime>
 #include <iostream>
-#include <boost/array.hpp>
+#include <string>
 #include <boost/asio.hpp>
 
+#pragma warning(disable : 4996)
+
+using boost::asio::ip::tcp;
+
+std::string make_daytime_string()
+{
+	using namespace std; // For time_t, time and ctime;
+	time_t now = time(0);
+	return ctime(&now);
+}
 
 int main()
 {
-    return 0;
-}
+	try
+	{
+		boost::asio::io_context io_context;
 
+		tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 13));
+
+		for (;;)
+		{
+			tcp::socket socket(io_context);
+			acceptor.accept(socket);
+
+			std::string message = make_daytime_string();
+
+			boost::system::error_code ignored_error;
+			boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
+		}
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	return 0;
+}
